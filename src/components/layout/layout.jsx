44795a2875ee329f8e57628c91b3e9991ea3c28c
Oladele Jesudamilola <116273872+DameeJ@users.noreply.github.com>
2026-06-07 04:50:15 -0700

    Add layout component with Sidebar, Header, and HeroSection

--- src/components/layout/layout.jsx
+++ src/components/layout/layout.jsx
@@ -1,1 +1,26 @@
+import Sidebar from "./Sidebar";
+import Header from "./Header";
+import HeroSection from "./HeroSection";
 
+export default function Layout() {
+  return (
+    <div
+      className="
+      min-h-screen
+      bg-slate-950
+      text-white
+      flex
+      "
+    >
+      <Sidebar />
+
+      <div className="flex-1 flex flex-col">
+        <Header />
+
+        <main className="flex-1 overflow-auto">
+          <HeroSection />
+        </main>
+      </div>
+    </div>
+  );
+}
