af49b5f589e60564a21a246c8309f0f5d884305c
Oladele Jesudamilola <116273872+DameeJ@users.noreply.github.com>
2026-06-07 05:58:10 -0700

    Add project estimation function for services

--- src/services/Estimator.js
+++ src/services/Estimator.js
@@ -1,1 +1,23 @@
+// services/Estimator.js
 
+import { electricalRules }
+from "./ElectricalRules";
+
+import { solarRules }
+from "./SolarRules";
+
+export function estimateProject(data){
+
+  switch(data.service){
+
+    case "Electrical Wiring":
+      return estimateElectrical(data);
+
+    case "Solar Installation":
+      return estimateSolar(data);
+
+    default:
+      return null;
+  }
+
+}
