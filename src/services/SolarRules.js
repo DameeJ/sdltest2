602774677466d2dcfd07f6cfd69df9d296f1767f
Oladele Jesudamilola <116273872+DameeJ@users.noreply.github.com>
2026-06-07 05:57:05 -0700

    Update SolarRules.js

--- src/services/SolarRules.js
+++ src/services/SolarRules.js
@@ -1,1 +1,29 @@
+// services/SolarRules.js
 
+export const solarRules = {
+
+  smallHome: {
+    batteryCapacity: "5kWh",
+    inverter: "3.5kVA",
+    panels: 4
+  },
+
+  mediumHome: {
+    batteryCapacity: "10kWh",
+    inverter: "5kVA",
+    panels: 8
+  },
+
+  largeHome: {
+    batteryCapacity: "15kWh",
+    inverter: "10kVA",
+    panels: 12
+  },
+
+  commercial: {
+    batteryCapacity: "30kWh",
+    inverter: "20kVA",
+    panels: 24
+  }
+
+};
